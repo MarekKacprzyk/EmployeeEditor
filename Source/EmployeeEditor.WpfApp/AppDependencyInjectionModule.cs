ee58b28f87f8b280e5dc60f2ba6ad9b78c95a1dc
SKYNET\kacpr <kacprzyk.marek@Gmail.com>
2023-07-12 22:49:56 +0200

    Add register IAppService's and extract specyfic register to method

--- Source/EmployeeEditor.WpfApp/AppDependencyInjectionModule.cs
+++ Source/EmployeeEditor.WpfApp/AppDependencyInjectionModule.cs
@@ -1,10 +1,13 @@
 ﻿using System.Linq;
+using System.Reflection;
 using Autofac;
 using Caliburn.Micro;
 using EmployeeEditor.Database;
 using EmployeeEditor.Domain;
+using EmployeeEditor.Domain.Interfaces;
 using EmployeeEditor.WpfApp.ViewModels;
 using EmployeeEditor.WpfApp.Views;
+using Module = Autofac.Module;
 
 namespace EmployeeEditor.WpfApp;
 
@@ -23,21 +26,45 @@ public class AppDependencyInjectionModule : Module
             .As<IWindowManager>()
             .SingleInstance();
 
-        // Rejestruj widoki
-        builder.RegisterAssemblyTypes(AssemblySource.Instance.ToArray())
-            .Where(type => type.Name.EndsWith("View"))
-            .Except<MainWindowView>()
-            .AsSelf();
+        var types = AssemblySource.Instance.ToArray();
 
-        // Rejestruj viewmodele
-        builder.RegisterAssemblyTypes(AssemblySource.Instance.ToArray())
+        RegisterServices(builder, types);
+        RegisterViewModels(builder, types);
+        RegisterViews(builder, types);
+    }
+
+    private static void RegisterViewModels(ContainerBuilder builder, Assembly[] asemblyTypes)
+    {
+        builder.RegisterAssemblyTypes(asemblyTypes)
             .Where(type => type.Name.EndsWith("ViewModel"))
             .Except<MainWindowViewModel>()
             .AsSelf()
             .InstancePerDependency();
 
-        builder.RegisterTypes(typeof(MainWindowView), typeof(MainWindowViewModel))
+
+        builder.RegisterTypes(typeof(MainWindowViewModel))
             .AsSelf()
             .SingleInstance();
+    }
+
+    private static void RegisterViews(ContainerBuilder builder, Assembly[] types)
+    {
+        builder.RegisterAssemblyTypes(types)
+            .Where(type => type.Name.EndsWith("View"))
+            .Except<MainWindowView>()
+            .AsSelf();
+
+        builder.RegisterTypes(typeof(MainWindowView))
+            .AsSelf()
+            .SingleInstance();
+    }
+
+    private static void RegisterServices(ContainerBuilder builder, Assembly[] types)
+    {
+        builder.RegisterAssemblyTypes(types)
+            .Where(type => typeof(IAppService).IsAssignableFrom(type))
+            .Except<MainWindowViewModel>()
+            .AsSelf()
+            .InstancePerDependency();
     }
 }
