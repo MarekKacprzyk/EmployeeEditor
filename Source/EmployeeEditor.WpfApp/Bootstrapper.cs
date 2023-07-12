b6d6257191604a65a24066e294824aee16fb0d79
SKYNET\kacpr <kacprzyk.marek@Gmail.com>
2023-07-12 21:36:38 +0200

    Add Caliburn.micro bootstrap and autofac

---
+++ Source/EmployeeEditor.WpfApp/Bootstrapper.cs
@@ -0,0 +1,77 @@
+﻿using Autofac;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using EmployeeEditor.WpfApp.ViewModels;
+
+namespace EmployeeEditor.WpfApp
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private IContainer _container = null!;
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<EventAggregator>()
+                .As<IEventAggregator>()
+                .SingleInstance();
+
+            builder.RegisterType<WindowManager>()
+                .As<IWindowManager>()
+                .SingleInstance();
+
+            // Rejestruj widoki
+            builder.RegisterAssemblyTypes(AssemblySource.Instance.ToArray())
+                .Where(type => type.Name.EndsWith("View"))
+                .AsSelf();
+
+            // Rejestruj viewmodele
+            builder.RegisterAssemblyTypes(AssemblySource.Instance.ToArray())
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .AsSelf()
+                .InstancePerDependency();
+            
+            
+            
+            _container = builder.Build();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<MainWindowViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                if (_container.IsRegistered(service))
+                    return _container.Resolve(service);
+            }
+
+            if (_container.IsRegisteredWithKey(key, service))
+                return _container.ResolveKeyed(key, service);
+
+            throw new Exception($"Could not locate any instances of contract {key ?? service.Name}.");
+        }
+
+        protected override IEnumerable<object>? GetAllInstances(Type service)
+        {
+            return _container.Resolve(typeof(IEnumerable<>).MakeGenericType(service)) as IEnumerable<object>;
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.InjectProperties(instance);
+        }
+    }
+}
